cf8abff3673ca715450f7fe7bda8744fecde066e
Chinmoy Biswas <chinmoybiswas765@gmail.com>
2021-07-21 17:31:00 +0600

    add collectionView

--- PhotoEditor/Controllers/EditorViewController.swift
+++ PhotoEditor/Controllers/EditorViewController.swift
@@ -21,41 +21,86 @@ class EditorViewController: UIViewController {
         button.setTitle("Filter", for: .normal)
         return button
     }()
+    
+    let filters = ["CIBumpDistortion", "CIGaussianBlur", "CIPixellate", "CISepiaTone", "CITwirlDistortion", "CIUnsharpMask", "CIVignette"]
+    
+//    private var filterCollectionView: UICollectionView = {
+//        let collectionView = UICollectionView()
+//        collectionView.register(FilterCollectionViewCell.self, forCellWithReuseIdentifier: FilterCollectionViewCell.identifier)
+//        return collectionView
+//    }()
+    
+    private var filterCollectionView: UICollectionView = {
+        let layout = UICollectionViewFlowLayout()
+        layout.scrollDirection = .horizontal
+        layout.minimumLineSpacing = 15
+        let cv = UICollectionView(frame: .zero, collectionViewLayout: layout)
+        cv.register(FilterCollectionViewCell.self, forCellWithReuseIdentifier: FilterCollectionViewCell.identifier)
+        cv.translatesAutoresizingMaskIntoConstraints = false
+        
+        return cv
+    }()
 
 
     override func viewDidLoad() {
         super.viewDidLoad()
         view.backgroundColor = #colorLiteral(red: 0.1529217958, green: 0.1529547274, blue: 0.1529174745, alpha: 1)
         
-        filterButton.addTarget(self,
-                              action: #selector(filterButtonTapped),
-                              for: .touchUpInside)
+        setupNavigationBar()
+        setupCollectionView()
+        
+//        filterButton.addTarget(self,
+//                              action: #selector(filterButtonTapped),
+//                              for: .touchUpInside)
+
 
         
         // Add SubView
         view.addSubview(imageView)
         view.addSubview(filterButton)
+        view.addSubview(filterCollectionView)
+        print(imageView.bottom)
+        filterCollectionView.topAnchor.constraint(equalTo: view.topAnchor, constant: (view.height/7 + view.height/1.5)+20).isActive = true
+        filterCollectionView.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: 5).isActive = true
+        filterCollectionView.trailingAnchor.constraint(equalTo: view.trailingAnchor, constant: 0).isActive = true
+        filterCollectionView.heightAnchor.constraint(equalToConstant: view.frame.width/4).isActive = true
+
     }
     
     override func viewDidLayoutSubviews() {
         super.viewDidLayoutSubviews()
-        
-        title = "Eidt"
-        navigationController?.navigationBar.prefersLargeTitles = false
-        navigationController?.navigationBar.barTintColor = #colorLiteral(red: 0.1529218853, green: 0.1529548466, blue: 0.1572185457, alpha: 1)
-        navigationController?.navigationBar.tintColor = .white
-        navigationController?.navigationBar.titleTextAttributes = [.foregroundColor: UIColor.white]
-
         
         imageView.frame = CGRect(x: 0,
                                  y: view.height/7,
                                  width: view.width,
                                  height: view.height/1.5)
         
-        filterButton.frame = CGRect(x: (view.width/2)-20,
-                                    y: imageView.bottom+10,
-                                    width: 40,
-                                    height: 20)
+//        filterButton.frame = CGRect(x: (view.width/2)-20,
+//                                    y: imageView.bottom+10,
+//                                    width: 40,
+//                                    height: 20)
+ 
+
+    }
+    
+    private func setupNavigationBar() {
+        title = "Eidt"
+        navigationController?.navigationBar.prefersLargeTitles = false
+        navigationController?.navigationBar.barTintColor = #colorLiteral(red: 0.1529218853, green: 0.1529548466, blue: 0.1572185457, alpha: 1)
+        navigationController?.navigationBar.tintColor = .white
+        navigationController?.navigationBar.titleTextAttributes = [.foregroundColor: UIColor.white]
+    }
+    
+    private func setupCollectionView() {
+//        let layout = UICollectionViewFlowLayout()
+//        layout.scrollDirection = .horizontal
+//        filterCollectionView?.isScrollEnabled = true
+//        filterCollectionView = UICollectionView(frame: .zero, collectionViewLayout: layout)
+//        filterCollectionView!.register(FilterCollectionViewCell.self, forCellWithReuseIdentifier: FilterCollectionViewCell.identifier)
+        
+
+        filterCollectionView.delegate = self
+        filterCollectionView.dataSource = self
     }
     
     // apply sepia filter
@@ -76,4 +121,21 @@ class EditorViewController: UIViewController {
     }
     
 
+}
+
+extension EditorViewController: UICollectionViewDelegate, UICollectionViewDataSource, UICollectionViewDelegateFlowLayout {
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return filters.count
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: FilterCollectionViewCell.identifier, for: indexPath) as! FilterCollectionViewCell
+        cell.editedImage.image = imageView.image
+        return cell
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAt indexPath: IndexPath) -> CGSize {
+        return CGSize(width: (filterCollectionView.frame.width)/4.5, height: (filterCollectionView.frame.width)/4)
+    }
+    
 }
---
+++ PhotoEditor/Views/FilterCollectionViewCell.swift
@@ -0,0 +1,47 @@
+//
+//  FilterCollectionViewCell.swift
+//  PhotoEditor
+//
+//  Created by Chinmoy Biswas on 21/7/21.
+//
+
+import UIKit
+
+class FilterCollectionViewCell: UICollectionViewCell {
+    
+    static let identifier = "FilterCollectionViewCell"
+    
+    let editedImage: UIImageView = {
+        let imageView = UIImageView()
+        imageView.layer.cornerRadius = 5
+        return imageView
+    }()
+    
+    override init(frame: CGRect) {
+        super.init(frame: .zero)
+        
+        contentView.addSubview(editedImage)
+        
+        editedImage.frame = CGRect(x: 0, y: 0, width: 100, height: 100)
+        
+        editedImage.topAnchor.constraint(equalTo: contentView.topAnchor).isActive = true
+        editedImage.leftAnchor.constraint(equalTo: contentView.leftAnchor).isActive = true
+        editedImage.rightAnchor.constraint(equalTo: contentView.rightAnchor).isActive = true
+        editedImage.bottomAnchor.constraint(equalTo: contentView.bottomAnchor).isActive = true
+
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+//    override func layoutSubviews() {
+//        super.layoutSubviews()
+//
+//        editedImage.frame = CGRect(x: 10,
+//                                     y: 10,
+//                                     width: 100,
+//                                     height: 100)
+//
+//    }
+}
